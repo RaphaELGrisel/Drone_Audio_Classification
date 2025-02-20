53d0439fd3e2fd39c008d24162d1a5284a1c3b39
rapha <raphael.grisel@ensta-bretagne.org>
2025-02-20 15:03:29 +0100

    training pipeline

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -6,8 +7,9 @@ from model import Model
 
 
 class Train():
-    def __init__(self,data_dir):
+    def __init__(self,data_dir,n_epochs):
         self.data_path = data_dir
+        self.n_epochs = n_epochs
 
     def datasets(self,train_split=0.7,test_split=0.15,val_split=0.15):
 
@@ -27,3 +29,54 @@ class Train():
         test_ds = test_ds.cache().prefetch(tf.data.AUTOTUNE)
 
         return train_ds, val_ds, test_ds
+    
+
+    def training_loop(self,train_ds,val_ds):
+        model1 = Model(train_ds)
+        model = model1.CNN(2)
+        model.compile(
+            optimizer = tf.keras.optimizes.Adam(),
+            loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
+        n = self.n_epochs
+        history = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=n,
+            callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2)
+        )
+        metrics = history.history
+        plt.figure(figsize=(16,6))
+        plt.subplot(1,2,1)
+        plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+        plt.legend(['loss', 'val_loss'])
+        plt.ylim([0, max(plt.ylim())])
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss [CrossEntropy]')
+
+        plt.subplot(1,2,2)
+        plt.plot(history.epoch, 100*np.array(metrics['accuracy']), 100*np.array(metrics['val_accuracy']))
+        plt.legend(['accuracy', 'val_accuracy'])
+        plt.ylim([0, 100])
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy [%]')     
+
+        return model 
+    
+    @staticmethod
+    def save_model(trained_model,model_name,saving_path="saved_model"):
+        path = os.path.join(saving_path,model_name)
+        trained_model.save(path)
+
+    @staticmethod
+    def load_model(model_name,saving_path="saved_model"):
+        path = os.path.join(saved_model,model_name)
+        model = keras.models.load_model(path)
+        return model
+
+
+
+
+
+
