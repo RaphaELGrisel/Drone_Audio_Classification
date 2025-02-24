0d572528c4482e6c0da40c65d65a1212730b6539
rapha <raphael.grisel@ensta-bretagne.org>
2025-02-24 13:30:13 +0100

    testing methods

--- src/test.py
+++ src/test.py
@@ -3,14 +3,21 @@ import numpy as np
 
 from data_processing import DataProcessing
 from model import Model
+import os
 
 file = "/home/raphalinux/PycharmProjects/pythonProject/projet_sys/data/Binary_Drone_Audio"
 dataset_binary = DataProcessing(file)
-#dataset_binary.plot_waveform(4)
-#dataset_binary.plot_waveform(2,"unknown")
-#dataset_binary.plot_spectrogram(1)
-#dataset_binary.plot_spectrogram(1,"unknown")
+#EXECUTE ONLY ONCE !!!
+#dataset_binary.select_dataset_part("unknown")
+#EXECUTE ONLY ONCE !!! 
+path_class = os.path.join(file,"unknown")
+files_after = [f for f in os.listdir(path_class)]
+print(f"Size file after {len(files_after)}")
+
 spectro_dataset = dataset_binary.get_spectrogram_dataset()
+print(spectro_dataset.type)
+
+"""
 
 for example_spectrograms, example_spect_labels in spectro_dataset.take(1):
   break
@@ -26,3 +33,5 @@ print("#-----------------------------#")
 model1 = Model(spectro_dataset)
 cnn = model1.CNN((124,129,1),2)
 cnn.summary()
+
+"""
