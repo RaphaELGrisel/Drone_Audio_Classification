d590eb09ebe3263d10197d34a70f9042fd361548
rapha <raphael.grisel@ensta-bretagne.org>
2025-02-27 16:44:57 +0100

    LSTM Model

--- src/model.py
+++ src/model.py
@@ -39,4 +39,19 @@ class Model():
         model.summary()
         return model
     
+    def LSTM(self,n_labels = 2):
+        
+        input_dim = self.train_spectrogram.element_spec[0].shape[:-1]
+        print(input_dim)
+        model = models.Sequential([
+            layers.Input(shape=input_dim),
+            layers.LSTM(128),
+            layers.LSTM(64),
+            layers.Dense(128,activation="relu"),
+            layers.Dense(64,activation="relu"),
+            layers.Dense(n_labels)
+        ])
+        
+        model.summary()
+        return model
     
