b8c8c84e49a389e8f315d128851ea41edb76df31
rapha <raphael.grisel@ensta-bretagne.org>
2025-02-27 13:58:43 +0100

    add some layers

--- src/model.py
+++ src/model.py
@@ -13,23 +13,25 @@ class Model():
     def CNN(self,n_labels = 2 ,input_dim=(124,129,1)):
 
         """
-        When using Mel spectrogram, comment normalisation layer
+        If normalization done in the data preprocessing, then the normalization layer is not needed.
         
         """
         input_shape = input_dim
-        #norm_layer = layers.Normalization()
-        #spectrogram_ds = self.train_spectrogram.map(lambda spec, label: spec)
-        #norm_layer.adapt(spectrogram_ds)
+        norm_layer = layers.Normalization()
+        spectrogram_ds = self.train_spectrogram.map(lambda spec, label: spec)
+        norm_layer.adapt(spectrogram_ds)
         model = models.Sequential([
             layers.Input(shape=input_shape),
             layers.Resizing(32,32),
-            #norm_layer,
+            norm_layer,
             layers.Conv2D(32,3,activation="relu"),
             layers.Conv2D(64,3,activation="relu"),
+            layers.Conv2D(128,3,activation="relu"),
             layers.MaxPooling2D(),
             layers.Dropout(0.25),
             layers.Flatten(),
             layers.Dense(128,activation="relu"),
+            layers.Dense(64,activation="relu"),
             layers.Dropout(0.5),
             layers.Dense(n_labels)
         ])
